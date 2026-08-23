5f5d69a8e9d51171998a1e70d0853d38a830f709
RatazanaEXE <claudia.r.carvalho2015@gmail.com>
2026-08-23 18:26:31 -0300

    Criação do método salvar.

---
+++ src/main/java/GatoRepository.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GatoRepository {
+     private List<Gato> bancoDeDados = new ArrayList<>();
+
+     public void Salvar (Gato gato){
+         bancoDeDados.add(gato);
+         System.out.println("Gato: " + gato.getNome() + " cadastrado com sucesso!");
+     }
+
+
+}
